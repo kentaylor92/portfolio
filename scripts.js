035393ec9381180413a383304d1c32b60e6ed023
kentaylor92 <ken_seaway@gmail.com>
2020-03-20 12:48:08 -0400

    inital html/css/js setup.  started scss styling

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+    
+    $('.headerText').ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.04,
+    
+    });
+
+    $(".bounce").click(function () {
+        bounce($(this), 3, '10px', 300);
+    });
+
+
+
+    function bounce(element, times, distance, speed) {
+        for (i = 0; i < times; i++) {
+            element.animate({ marginTop: '-=' + distance }, speed)
+                .animate({ marginTop: '+=' + distance }, speed);
+        }
+    }
+
+});      
+
+
+
+
