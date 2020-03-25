a16ae6254b86b0797b6984db12d262b4ec2c9b11
kentaylor92 <ken_seaway@gmail.com>
2020-03-25 13:26:21 -0400

    added intersection observer scrolling, edited css for large-desktops, reconfigured sidebar links

--- scripts.js
+++ scripts.js
@@ -9,6 +9,55 @@ $(document).ready(function () {
     
     });
 
+    const scrollDown = document.querySelectorAll('.anim');
+    const scrollLeft = document.querySelectorAll('.scrollLeft');
+    const scrollRight = document.querySelectorAll('.scrollRight');
+
+    observer = new IntersectionObserver((entries) => {
+        
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.style.animation = `anim1 1s ${entry.target.dataset.delay} forwards ease-in-out`;
+            } else {
+                entry.target.style.animation = `none`;
+            }
+        })
+
+        
+    })
+    scrollDown.forEach(image => {
+        observer.observe(image);
+    })
+
+
+    observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.style.animation = `anim2 1s ${entry.target.dataset.delay} forwards ease-out`;
+            } else {
+                entry.target.style.animation = `none`;
+            }
+        })
+    })
+    scrollLeft.forEach(image => {
+        observer.observe(image);
+    });
+
+    observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.style.animation = `anim3 1s ${entry.target.dataset.delay} forwards ease-out`;
+            } else {
+                entry.target.style.animation = `none`;
+            }
+        })
+    })
+    scrollRight.forEach(image => {
+        observer.observe(image);
+    });
+
     
     
 
