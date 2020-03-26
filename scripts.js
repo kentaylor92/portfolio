6354a739b6e51026ea5178214df711d365e08289
kentaylor92 <ken_seaway@gmail.com>
2020-03-26 15:52:06 -0400

    started responsiveness/media queries, shrunk image sizes, configured hamburger menu

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 
 
 $(document).ready(function () {
+
+    const menuBtn = document.querySelector('.menu-btn');
+    const navBar = document.querySelector('.navBar');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if(!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+        }
+    });
+
     
     $('.headerText').ripples({
         resolution: 512,
